5875c49525ed40d71a3d821469357b06c60f0674
Marcos Mora <abreu760@hotmail.com>
2025-08-27 12:43:26 -0400

    fix: app functional with redux provider and dependencies

--- learning_react/src/main.jsx
+++ learning_react/src/main.jsx
@@ -1,9 +1,8 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import './App.css';
+
 import { Provider } from 'react-redux';
 import store from './store';
 
